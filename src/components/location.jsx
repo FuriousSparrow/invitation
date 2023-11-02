394015cae7527f7012421d28dc787be6b70e8dec
FuriousSparrow <149667487+FuriousSparrow@users.noreply.github.com>
2023-11-02 23:02:26 +0900

    Update location.jsx

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -47,8 +47,8 @@ const Location = () => {
   const executeScript = () => {
     const scriptTag = document.createElement("script");
     const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-		"timestamp" : "1698932191605",
-		"key" : "2gpb4",
+		"timestamp" : "1698933482992",
+		"key" : "2gpb6",
 		"mapWidth" : "640",
 		"mapHeight" : "360"
   }).render();`);
