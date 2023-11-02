7b1b3d55063cf74407d4999d1b924d14aa338107
FuriousSparrow <149667487+FuriousSparrow@users.noreply.github.com>
2023-11-02 22:43:24 +0900

    Update location.jsx

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -47,15 +47,18 @@ const Location = () => {
   const executeScript = () => {
     const scriptTag = document.createElement("script");
     const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-    "timestamp" : "1652464367301",
-    "key" : "2a8fe",
-    "mapWidth" : "640",
-    "mapHeight" : "360"
+		"timestamp" : "1698932191605",
+		"key" : "2gpb4",
+		"mapWidth" : "640",
+		"mapHeight" : "360"
   }).render();`);
     scriptTag.appendChild(inlineScript);
     document.body.appendChild(scriptTag);
   };
 
+
+
+  
   // <!-- 2. 설치 스크립트 * 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다. -->
   // document.write 문제가 발생해서 해당 파일을 직접 가져온다음 수정했음
   const InstallScript = () => {
@@ -103,23 +106,15 @@ const Location = () => {
         className="root_daum_roughmap root_daum_roughmap_landing"
       ></Map>
       <Content>
-        대구 수성구 두산동 888-2번지
+        서울 마포구 마포대로 92
         <br />
-        호텔수성 수성스퀘어 3층 피오니홀
-        <br />
-        <br />
-        <Title>버스 이용시</Title>
-        <br />
-        <br />
-        410-1, 401 호텔수성 앞 하차
-        <br />
-        수성1-1, 수성3-1, 814 TBC방송국 앞 하차
+        아펠가모 공덕점 마리에홀
         <br />
         <br />
         <Title>지하철 이용시</Title>
         <br />
         <br />
-        3호선 수성못역 하차 (도보 10분)
+        5,6호선 공항철도 경의중앙선 공덕역 하차 (도보 5분)
       </Content>
     </Wrapper>
   );
