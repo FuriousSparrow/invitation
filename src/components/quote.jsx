db41ada198d17a8be1410484e83651a072bff498
FuriousSparrow <149667487+FuriousSparrow@users.noreply.github.com>
2023-11-06 07:51:44 +0900

    Update quote.jsx

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -40,6 +40,7 @@ const Quote = () => {
       <Image src={Flower} data-aos="fade-up" />
       <Content data-aos="fade-up">
         <br />
+        <br />
         호기심 많은 장난꾸러기 권민이와
         <br />
         이러쿵 저러쿵 얼씨구나 혜경이는
